d55a489e4866e264da63d9e013f877b9ea5fa07b
ki11errabbit <ki11errabbit@posteo.net>
2024-03-21 16:31:11 -0600

    made the output window and nodes dragable

--- Command/Command.gd
+++ Command/Command.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 class_name Command
 @export var command: String
@@ -40,3 +40,35 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+var draggingDistance
+var dir
+var dragging
+var newPosition = Vector2()
+var mouse_in = false
+
+func mouse_entered():
+	mouse_in = true
+
+func mouse_exited():
+	mouse_in = false
+
+
+func _physics_process(delta):
+	if dragging:
+		velocity = (newPosition - position) * Vector2(30, 30)
+		move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed() && mouse_in:
+			draggingDistance = position.distance_to(get_viewport().get_mouse_position())
+			dir = (get_viewport().get_mouse_position() - position).normalized()
+			dragging = true
+			newPosition = get_viewport().get_mouse_position() - draggingDistance * dir
+		else:
+			dragging = false
+			
+	elif event is InputEventMouseMotion:
+		if dragging:
+			newPosition = get_viewport().get_mouse_position() - draggingDistance * dir
--- Output/Output.gd
+++ Output/Output.gd
@@ -1,6 +1,8 @@
-extends Control
+extends CharacterBody2D
 
 signal compile
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,3 +21,35 @@ func _on_texture_button_pressed():
 func set_text(str: String):
 	$CodeEdit.text = str
 	#$CodeEdit.set_code_hint("bash")
+
+var draggingDistance
+var dir
+var dragging
+var newPosition = Vector2()
+var mouse_in = false
+
+func mouse_entered():
+	mouse_in = true
+
+func mouse_exited():
+	mouse_in = false
+
+
+func _physics_process(delta):
+	if dragging:
+		velocity = (newPosition - position) * Vector2(30, 30)
+		move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed() && mouse_in:
+			draggingDistance = position.distance_to(get_viewport().get_mouse_position())
+			dir = (get_viewport().get_mouse_position() - position).normalized()
+			dragging = true
+			newPosition = get_viewport().get_mouse_position() - draggingDistance * dir
+		else:
+			dragging = false
+			
+	elif event is InputEventMouseMotion:
+		if dragging:
+			newPosition = get_viewport().get_mouse_position() - draggingDistance * dir
